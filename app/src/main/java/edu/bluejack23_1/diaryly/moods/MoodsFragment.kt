24345e52d441a614f5ba2b08d55efaf814d5120a
danielwidhiarto <danielwidhiarto@gmail.com>
2023-11-16 15:37:50 +0700

    realtime onsnapshot moods

--- app/src/main/java/edu/bluejack23_1/diaryly/moods/MoodsFragment.kt
+++ app/src/main/java/edu/bluejack23_1/diaryly/moods/MoodsFragment.kt
@@ -11,12 +11,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
 import edu.bluejack23_1.diaryly.R
 
 class MoodsFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var moodsAdapter: MoodsAdapter
+    private lateinit var snapshotListener: ListenerRegistration
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,6 +66,33 @@ class MoodsFragment : Fragment() {
                 .addOnFailureListener { exception ->
                     // Handle errors here
                 }
+
+            // Set up the snapshot listener for real-time updates
+            val query = db.collection("moods")
+                .whereEqualTo("userId", userId)
+                .orderBy("date", Query.Direction.DESCENDING)
+
+            snapshotListener = query.addSnapshotListener { snapshots, exception ->
+                if (exception != null) {
+                    // Handle errors here
+                    return@addSnapshotListener
+                }
+
+                moodList.clear() // Clear the existing list
+
+                for (document in snapshots!!) {
+                    val time = document.getString("time") ?: "No Title"
+                    val date = document.getString("date") ?: "No Date"
+                    val mood = document.getString("chosenMood") ?: "No Content"
+                    val notes = document.getString("notes") ?: "No Data"
+                    val id = document.id // Retrieve the document ID
+
+                    val moodEntry = Moods(id, date, time, mood, notes) // Update to include the ID
+                    moodList.add(moodEntry)
+                }
+
+                moodsAdapter.updateData(moodList) // Update adapter data
+            }
         }
 
         // Set an OnClickListener for the FAB
